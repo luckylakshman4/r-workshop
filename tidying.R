b080cfcdb72c0c3bc8673622c8d019ba642367ca
Lakshmana Rao C <lakshmanch93@gmail.com>
2022-12-03 17:18:51 +0530

    Tidying

---
+++ tidying.R
@@ -0,0 +1,24 @@
+
+# Tidying
+
+# Reshaping data frames (converting from long-to-wide, or wide-to-long format), 
+# separating and uniting variable (column) contents, and finding and replacing string patterns
+
+# Use tidyr::pivot_wider() and tidyr::pivot_longer() to reshape data frames
+# janitor::clean_names() to make column headers more manageable
+# tidyr::unite() and tidyr::separate() to merge or separate information from different columns
+# Detect or replace a string with stringr functions
+
+# install packages
+# install.packages("tidyverse")
+# install.packages("janitor")
+# install.packages("here")
+# install.packages("readxl")
+
+
+# Attach packages
+library(tidyverse)
+library(janitor)
+library(here)
+library(readxl)
+
